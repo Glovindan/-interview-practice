51bbd045f55f74f455a533107b9a1be74690fbcc
glovindan <turbo69nice@gmail.com>
2021-09-09 01:47:07 +0500

    refactor #1

--- JS/main.js
+++ JS/main.js
@@ -6,56 +6,61 @@ const addQuestion = function () {
 
 const addQuestionWithMarkup = function (markup) {
   const mainWrapper = $('.main-wrapper');
-  let questionMarkup = markup;
-  $(mainWrapper).children(':last-child').before(questionMarkup);
+  $(mainWrapper).children(':last-child').before(markup);
 }
 
-const deleteQuestion = function() {
-  const parent = $(this).parent().parent();
+function deleteQuestion() {
+  const parent = $(this).parents('.question-button-wrapper');
   $(parent).css("-webkit-animation","delete .3s");
-  $(parent).bind('webkitAnimationEnd',function(){
+  $(parent).one('webkitAnimationEnd',function(){
     $(parent).remove();
   });
 }
 
 const addAnswer = function () {
   let answerTextHTML = getAnswerMarkup();
-  $(this).before(answerTextHTML)
+  $(this).before($(answerTextHTML))
 
-  if($(this).parent().children(".answer-wrapper").length === 10) {
-    $(this).hide();
+  if($(this).parent().children(".answer-animation-wrapper").length === 10) {
+    $(this).css("-webkit-animation","fadeOut .3s");
+    $(this).one('webkitAnimationEnd',function(){
+      $(this).hide();
+    });
   }
 }
 const deleteAnswer = function() {
-  const parent = $(this).parent().parent().parent();
-  $(parent).css("-webkit-animation","delete .3s");
-  $(parent).bind('webkitAnimationEnd',function(){
-    $(parent).remove();
-  });
+  const answerWrapper = $(this).parents('.answer-animation-wrapper');
+  $(answerWrapper).css("-webkit-animation","delete .3s");
+  $(answerWrapper).one('webkitAnimationEnd',function(){
+    $(answerWrapper).remove();
+  })
 
-  if($(parent).parent().children(".answer-wrapper").length === 10) {
-    const button = $(parent).parent().children(".add-answer");//Некрасиво
+  if($(answerWrapper).parent().children(".answer-animation-wrapper").length === 10) {
+    const button = $(answerWrapper).parent().children(".add-answer");
     $(button).show();
+    $(button).css("-webkit-animation","fadeIn .3s");
   }
 
-  if($(parent).parent().children(".answer-wrapper").length === 1) {
-    const question = $(parent).parent().parent().parent().children('.delete-question-wrapper').children('.question-delete');//Некрасиво
+  if($(answerWrapper).parent().children(".answer-animation-wrapper").length === 1) {
+    const question = $(answerWrapper).parents('.question-button-wrapper').children().find('.question-delete');
     $(question).trigger('click');
   }
 }
 
 const getAnswerMarkup = function(text = "") {
   let answer = `
-    <div class="answer-wrapper">
-      <div class="question-answer">
-        <div class="checkbox-wrapper">
-          <div class="question-answer-checkbox"></div>
+    <div class="answer-animation-wrapper">
+      <div class="answer-wrapper">
+        <div class="answer-checkbox-wrapper">
+          <div class="answer-checkbox"></div>
         </div>
-        <div class="text-wrapper">
-          <div class="question-answer-text" contentEditable=true myPlaceholder="Введите вариант ответа">${text}</div>
+        
+        <div class="answer-text-wrapper">
+          <div class="answer-text" contentEditable=true myPlaceholder="Введите вариант ответа">${text}</div>
         </div>
-        <div class="delete-wrapper">
-          <div class="question-answer-delete unselectable">
+        
+        <div class="answer-delete-wrapper">
+          <div class="answer-delete unselectable">
               <img src="img/cancel.svg" alt="">
           </div>
         </div>
@@ -68,12 +73,11 @@ const getAnswerMarkup = function(text = "") {
 
 const getQuestionMarkup = function(title = "", answers = []) {
   let answer =
-    `<div class="another-wrapper">
+    `<div class="question-button-wrapper">
       <div class="question-wrapper">
         <div class="question-title" contentEditable=true myPlaceholder="Введите вопрос">${title}</div>
 
-        <div class="question-answer-wrapper">
-        
+        <div class="answer-button-wrapper">
           <div class="add-button add-answer unselectable">
               <div class="button-add-icon">+</div>
               <div class="button-text">Добавить вариант ответа</div>
@@ -82,7 +86,7 @@ const getQuestionMarkup = function(title = "", answers = []) {
         </div>
       </div>
 
-      <div class="delete-question-wrapper">
+      <div class="question-delete-wrapper">
         <div class="question-delete unselectable">
           <img src="img/trash.svg" alt="">
         </div>
@@ -93,7 +97,7 @@ const getQuestionMarkup = function(title = "", answers = []) {
 
   answers.forEach((answerText) => {
     let answerTextHTML = getAnswerMarkup(answerText);
-    $(htmlAnswer).children('.question-wrapper').children('.question-answer-wrapper').children('.add-answer').before(answerTextHTML);
+    $(htmlAnswer).children().find('.add-answer').before(answerTextHTML);
   })
 
   return htmlAnswer;
@@ -107,4 +111,4 @@ $(document).on('click','.add-question',addQuestion);
 $(document).on('click','.question-delete',deleteQuestion);
 
 $(document).on('click','.add-answer',addAnswer);
-$(document).on('click','.question-answer-delete',deleteAnswer);
+$(document).on('click','.answer-delete',deleteAnswer);
